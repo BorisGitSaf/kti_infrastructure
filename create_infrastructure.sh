dd16954fbf5cc7ef854e2374f287abe4d440f504
BorisGitSaf <75093810+BorisGitSaf@users.noreply.github.com>
2021-12-25 03:16:30 +0300

    Update create_infrastructure.sh
    don't kill me

--- create_infrastructure.sh
+++ create_infrastructure.sh
@@ -2,7 +2,6 @@ printf '%s %s\n' "Start deployment in" "$(date)"
 printf '%s\n' "Cloning infrastructure files from repo"
 git clone https://github.com/BorisGitSaf/kti_infrastructure.git
 cd kti_infrastructure
-rm -f create_infrastructure.sh
 printf '%s\n' "Cloning development files from repo"
 git clone https://github.com/BorisGitSaf/kti_development.git ./html/reg
 printf '%s %s\n' "Start building custom php-fpm image in" "$(date)"
